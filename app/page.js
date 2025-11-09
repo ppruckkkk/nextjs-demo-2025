18673913639f4200d7fa5777558d5921c43f9b0c
unknown <robokung14944@gmail.com>
2025-11-09 15:56:42 +0700

    product

--- app/page.js
+++ app/page.js
@@ -1,17 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import FormComponent from "@/components/FormComponent";
+import { Button } from "reactstrap";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const[products, setProducts] = useState([]);
+  const[filteredproducts, setFilteredProducts] = useState([]);
+
+  const getAllProduct = async () =>{
+    try{
+    const response = await fetch("https://dummyjson.com/products");
+
+    if(!response.ok) throw new Error("Failed")
+
+    const allproduct = await response.json() 
+    setProducts(allproduct.products);
+    setFilteredProducts(allproduct.products);
+    }catch(error){
+      setProducts([]);
+      setFilteredProducts([]);
+      console.log("error",error)
+    }
+  };
+  console.log("-products-",products)
+  console.log("-filteredproduct-",filteredproducts)
+
+  useEffect(()=>{
+    getAllProduct();
+  },[]);
+
+  const handleSearch = (text)=>{
+    const filterproduct = products.filter((p)=> p.title.toLowerCase().includes(text.toLowerCase()));
+    
+    setFilteredProducts(filterproduct);
+  }
   return (
     <div>
       <Header />
-      <FormComponent />
+      <FormComponent onSearch={handleSearch}/>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-10">
-        {/* Code */}
+        {filteredproducts.map((item) =>(
+          <div key = {item.title} className="flex flex-col items-center border border-gray-300 rounded-3xl hover:shadow-lg cursor-pointer"
+          onClick={()=>router.push(`/products/${item.id}`)}>
+            <img alt={item.title} src={item.thumbnail}/>
+            <p className = "text-[16px] ">{item.title}</p>
+            <p className = "text-[16px] ">{item.price}$</p>
+            <div className="flex-row">
+            </div>
+        
+          </div>
+        ))}
       </div>
     </div>
   );
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,5 +1,11 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+import { useRouter } from "next/navigation";
+
+
 function formatStars(score) {
   if (!score) score = 0;
 
@@ -13,12 +19,57 @@ function formatStars(score) {
 }
 
 
-export default function ProductDetail() {
+export default function ProductDetail(){
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  const [product,setProducts] = useState({});
+
+  const getProductByid= async ()=>{
+    try{
+
+      const response = await fetch(`https://dummyjson.com/products/${id}`);
+      const data = await response.json()
+      setProducts(data)
+    }catch(error){
+      setProducts({});
+    }
+  }
+  console.log("-product-",product)
+  useEffect(()=>{
+    getProductByid();
+  },[]);
+
   return (
     <div className="flex flex-column h-auto justify-center gap-4 m-auto p-[1rem] max-w-[600px]">
       <p className="text-2xl font-bold text-center">Details</p>
-
-      <div className="flex flex-column w-full gap-2 items-center"></div>
+      
+      <div className="flex flex-column w-full gap-2 items-center">
+        <img alt={product.title} src= {product.thumbnail} className="h-[200px] " />
+        <p className="text-xl font-bold text-center">{product.title}</p>
+        <p className="text-lg  text-center">Description: {product.description}</p>
+        <p className="text-lg">${product.price}</p>
+        { (Array.isArray(product.reviews)&& product.reviews.length >0 ) && (
+          <div className="w-full">
+            <p className="text-xl font-semibold">Review</p>
+            <div className="flex flex-col gap-3"> 
+              {product.reviews.map((item,index)=>(
+                <div key={index} className="border border-gray-200 rounded-xl p-3">
+                  <div className="flex items-center justify-between">
+                    <span className="font-bold">{item.reviewerName}</span>
+                    <span>
+                      {formatStars(item.rating) } ({item.rating})
+                    </span>
+                  </div>
+                  <p>{item.comment}</p>
+                  <p className="text-xs text-gray-500">{new Date(item.date).toLocaleDateString()}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <Button color ="primary" className="w-[30%] self-center" onClick={() => router.push("/")}> Back</Button>
     </div>
   );
 }
